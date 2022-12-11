3732f2b16e47be2308f0058f97f90e905247dc09
Asterisk-tech <54914794+Asterisk-tech@users.noreply.github.com>
2022-12-11 01:51:41 -0500

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -3,7 +3,7 @@ const {Events} = require('discord.js');
 
 module.exports = function (client, config) {
 	client.on(Events.MessageCreate, message => {
-		const match = message.content.match(new RegExp(`^${config.commandPrefix}weather (\\d+)`));
+		const match = message.content.match(new RegExp(`^${config.commandPrefix}weather (\\d{5})`));
 		if (match) {
 			// Use the library to search for the weather data by zip code
 			weather.find({search: match[1], degreeType: 'F'}, (error, result) => {
@@ -11,14 +11,20 @@ module.exports = function (client, config) {
 					console.log(error);
 				}
 
-				// Get the first result from the search (should be the only result)
-				const weatherData = result[0].current;
-				// Reply with the current temperature, humidity, windspeed, and weather condition in a list format
-				message.reply(`Here is the current weather information:
-    - Temperature: ${weatherData.temperature} degrees
-    - Humidity: ${weatherData.humidity}%
-    - Windspeed: ${weatherData.windspeed}
-    - Weather: ${weatherData.skytext}`);
+				// Check if the search returned any results
+				if (result.length > 0) {
+					// Get the first result from the search (should be the only result)
+					const weatherData = result[0].current;
+					// Reply with the current temperature, humidity, windspeed, and weather condition in a list format
+					message.reply(`Here is the current weather information:
+      - Temperature: ${weatherData.temperature} degrees
+      - Humidity: ${weatherData.humidity}%
+      - Windspeed: ${weatherData.windspeed}
+      - Weather: ${weatherData.skytext}`);
+				} else {
+					// If no results were returned, reply with an error message
+					message.reply('Invalid zip code. Please enter a valid 5-digit US zip code.');
+				}
 			});
 		}
 	});
