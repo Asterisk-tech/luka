bdeebe1cf1512e5dd1a2fd153541d682e757256e
Asterisk-tech <54914794+Asterisk-tech@users.noreply.github.com>
2022-12-11 03:01:50 -0500

    revert

--- plugins/reload.js
+++ plugins/reload.js
@@ -2,25 +2,23 @@ const {exec, spawn} = require('node:child_process');
 const {Events} = require('discord.js');
 
 module.exports = function (client, config) {
-client.on(Events.MessageCreate, message => {
-	if (message.content === `${config.commandPrefix}reload` && message.author.id == config.userId) {
-	// Do a Git pull to check for updates
-		const gitPull = spawnSync('git', ['pull']);
+	client.on(Events.MessageCreate, message => {
+		if (message.content === `${config.commandPrefix}reload` && message.author.id == config.userId) {
+			// Do a Git pull to check for updates
+			const gitPull = spawn('git', ['pull']);
 
-		// If the exit code is 0, updates were installed
-		if (gitPull.status === 0) {
-			const killProcess = spawn('kill', [process.pid]);
-			killProcess.on('close', () => {
-				const nodeProcess = fork('./index.js');
-				nodeProcess.on('close', () => {
-					console.log('Node process restarted');
-					message.reply('Updates were installed, restarting...');
-				});
+			gitPull.stdout.on('data', data => {
+				// If there are any updates, restart the process
+				if (data.toString().includes('Already up to date.')) {
+					message.reply('Already up to date!');
+				} else {
+					exec('node ./index.js', (error, stdout, stderr) => {
+						if (error) {
+							console.error(`Error: ${error}`);
+						}
+					});
+				}
 			});
-		} else {
-			message.reply('Already up to date, you idiot!');
 		}
-	}
-});
-
+	});
 };
